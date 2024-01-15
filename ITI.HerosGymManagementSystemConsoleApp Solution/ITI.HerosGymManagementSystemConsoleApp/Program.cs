6fe6c474720f75b8462922fcdb49b6c34d611682
Mostafa Ahmed Mostafa <108753275+Mostafa-ElMonateh@users.noreply.github.com>
2024-01-15 03:11:33 +0200

    Initial Create

---
+++ ITI.HerosGymManagementSystemConsoleApp Solution/ITI.HerosGymManagementSystemConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ITI.HerosGymManagementSystemConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
